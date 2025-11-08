329102878d0ef63685ec21f271deb2e3908bd000
DavidFlorezQuin <davidmauricioflorez@gmail.com>
2025-11-08 11:36:30 -0500

    creacion login

--- frontend/store-web/src/app/app.routes.ts
+++ frontend/store-web/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './features/auth/login/login.component';
+import { ProductList } from './features/products/pages/product-list/product-list';
 
 export const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: '**', redirectTo: 'login' }
+    { path: 'login', component: LoginComponent },
+    { path: 'products', component: ProductList },
+    { path: '**', redirectTo: 'login' }
 ];
