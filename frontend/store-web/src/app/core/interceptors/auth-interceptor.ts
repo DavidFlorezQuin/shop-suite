75474ae08fe09b85c82ee955dca264cde4670fa9
David Florez <122068416+DavidFlorezQuin@users.noreply.github.com>
2025-11-09 15:55:07 -0500

    delete-logs

--- frontend/store-web/src/app/core/interceptors/auth-interceptor.ts
+++ frontend/store-web/src/app/core/interceptors/auth-interceptor.ts
@@ -2,7 +2,6 @@ import { HttpInterceptorFn } from '@angular/common/http';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const token = localStorage.getItem('token');
-  console.log('TOKEN ENVIADO:', token);
   if (token) {
     req = req.clone({
       setHeaders: { Authorization: `Bearer ${token}` }
